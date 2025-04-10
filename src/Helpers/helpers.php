ba1315ed36a8d6d349ce536f58b1397f1743178d
Hrybok Alex <zagadum@ukr.net>
2025-04-10 17:41:44 +0300

    models , db , migraion

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,5 +1,10 @@
 <?php
-
+if (!function_exists('format_date')) {
+    function format_date($date, $format = 'd.m.Y')
+    {
+        return \Carbon\Carbon::parse($date)->format($format);
+    }
+}
 //require_once __DIR__ . '/DateHelper.php';
 //require_once __DIR__ . '/StringHelper.php';
 //require_once __DIR__ . '/ArrayHelper.php';
