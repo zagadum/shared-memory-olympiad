b73c48f0243fbf75f915d8f7998591f3b67c0549
Hrybok Alex <zagadum@ukr.net>
2025-05-01 18:58:00 +0300

    update

---
+++ src/Models/Olympiad/MOlympiadParam.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MemoryOlympiad\Models\Olympiad;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MOlympiadParam extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'memory_olympiad';
+    protected $table = 'm_olympiad_params';
+
+    protected $fillable = [
+        'olympiad_id',
+        'stages_level',
+        'age_tab',
+        'stages_num',
+        'is_basic',
+        'is_intermediate',
+        'is_pro',
+        'training_type_id',
+        'params_json'
+    ];
+
+    protected $casts = [
+        'stages_level' => 'string',
+        'age_tab' => 'string',
+        'params_json' => 'array',
+    ];
+
+    // Связь с моделью MOlympiad
+    public function olympiad()
+    {
+        return $this->belongsTo(MOlympiad::class, 'olympiad_id');
+    }
+}
